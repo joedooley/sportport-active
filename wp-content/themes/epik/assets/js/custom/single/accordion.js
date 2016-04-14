e2c2560866572355293affbe29148d77407cb793
Joe Dooley <hello@developingdesigns.com>
2016-04-14 16:05:07 -0400

    Cleaned up JavaScript in accodion.js and site.js

--- wp-content/themes/epik/assets/js/custom/single/accordion.js
+++ wp-content/themes/epik/assets/js/custom/single/accordion.js
@@ -1,44 +1,25 @@
-//(function (document, $, undefined) {
-//
-//	'use strict';
-//
-//	$('.accordion').click(function () {
-//		if ($(this).parent().is('.open')) {
-//			$(this).closest('.accordion').find('.accordion-content').animate({'height': '0'}, 500);
-//			$(this).closest('.accordion').removeClass('open');
-//			$(this).parent().find('.accordion-icon').removeClass('fa-minus').addClass('fa-plus');
-//		}
-//		else {
-//			var newHeight = $(this).closest('.accordion').find('.accordion-container').height() + 'px';
-//			$(this).closest('.accordion').find('.accordion-container').animate({'height': newHeight}, 500);
-//			$(this).closest('.accordion').addClass('open');
-//			$(this).parent().find('.accordion-icon').removeClass('fa-plus').addClass('fa-minus');
-//		}
-//	});
-//
-//})(document, jQuery);
+(function ($, window, document, undefined) {
 
+	// http://codepen.io/joshhunt18/pen/JLtFq
 
-(function (document, $, undefined) {
+	'use strict';
 
-	var accordionItems = new Array();
-
-// Grab the accordion items from the page
+	// Grab the accordion items from the page
 	var accordionItems    = document.querySelectorAll('.accordion-item'),
 	    accordionContent  = document.querySelectorAll('.accordion-content');
 
-// Assign onclick events to the accordion item headings
+	// Assign onclick events to the accordion item headings
 	for (var i = 0; i < accordionItems.length; i++) {
 		var h2     = getFirstChildWithTagName(accordionItems[i], 'H2');
 		h2.onclick = toggleItem;
 	}
 
-// Hide all accordion item bodies except the first
+	// Hide all accordion item bodies except the first
 	for (var i = 1; i < accordionItems.length; i++) {
 		accordionItems[i].className = 'accordion-item closed';
 	}
 
-// Add exact height to div that is shown on page load
+	// Add exact height to div that is shown on page load
 	var firstDiv          = accordionItems[0].getElementsByTagName('div')[0];
 	firstDiv.style.height = "auto";
 	var firstDivHeight    = firstDiv.offsetHeight;
@@ -71,25 +52,26 @@
 		}
 	}
 
-// Helper function
+	// Helper function
 	function getFirstChildWithTagName(element, tagName) {
 		for (var i = 0; i < element.childNodes.length; i++) {
 			if (element.childNodes[i].nodeName == tagName) return element.childNodes[i];
 		}
 	}
 
+	// @TODO
+	// From FAQ-Plugin in Tonya's class
+	//function closeAnswersOnPageLoad() {
+	//
+	//	$.each($questions, function (index, element) {
+	//		var $question = $(element);
+	//
+	//		if (isAnswerToBeClosed($question)) {
+	//			var $answer = getAnswer(index);
+	//
+	//			$answer.hide();
+	//		}
+	//	});
+	//}
 
-	function closeAnswersOnPageLoad() {
-
-		$.each($questions, function (index, element) {
-			var $question = $(element);
-
-			if (isAnswerToBeClosed($question)) {
-				var $answer = getAnswer(index);
-
-				$answer.hide();
-			}
-		});
-	}
-
-})(document, jQuery);
+})(jQuery, window, document);
--- wp-content/themes/epik/assets/js/custom/site.js
+++ wp-content/themes/epik/assets/js/custom/site.js
@@ -1,25 +1,10 @@
-(function (document, $, undefined) {
+(function ($, window, document, undefined) {
 
-	$('.single-product div.product .product_title').click(function () {
-		$('.sku').toggle();
-	})
-
-
-
-
-})(document, jQuery);
-
-
-jQuery(function ($) {
-
-	// Quantity buttons
-	// $('div.quantity:not(.buttons_added), td.quantity:not(.buttons_added)').addClass('buttons_added').append('<input type="button" value="+" class="plus"
-	// />').prepend('<input type="button" value="-" class="minus" />');
-
+	'use strict';
 
 	$('div.quantity:not(.buttons_added), td.quantity:not(.buttons_added)').append('<div class="inc button buttons_added">+</div><div class="dec button buttons_added">-</div>');
 
-	$(".buttons_added").on("click", function () {
+	$('.buttons_added').on('click', function () {
 
 		var $button  = $(this);
 		var oldValue = $button.parent().find("input").val();
@@ -39,7 +24,7 @@ jQuery(function ($) {
 	});
 
 	// Use to add AJAX
-	
+
 	//var id = $button.attr("id");
 	//$.ajax({
 	//	type: "POST",
@@ -49,7 +34,7 @@ jQuery(function ($) {
 	//	}
 	//});
 
-});
+})(jQuery, window, document);
 
 
 
