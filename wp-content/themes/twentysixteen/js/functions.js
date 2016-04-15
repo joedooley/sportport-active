6c6a822c57662e70fa3e83a4b2dce44f79ea7f5f
Joe Dooley <hello@developingdesigns.com>
2016-04-15 12:13:06 -0400

    Revert "Updated Twentyxxxx themes"
    This reverts commit 480be77d5b6c4e043a49cdd5bb32a92197b40232.

--- wp-content/themes/twentysixteen/js/functions.js
+++ wp-content/themes/twentysixteen/js/functions.js
@@ -160,7 +160,7 @@
 					newImg = new Image();
 					newImg.src = element.attr( 'src' );
 
-					$( newImg ).on( 'load.twentysixteen', function() {
+					$( newImg ).load( function() {
 						if ( newImg.width >= 840  ) {
 							element.addClass( 'below-entry-meta' );
 
