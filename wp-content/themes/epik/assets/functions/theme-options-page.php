aa8b47e2b20c985fb85f51ce77f96de359c690e2
Joe Dooley <hello@developingdesigns.com>
2016-04-21 09:52:17 -0400

    Cleaned up code in before-header section

--- wp-content/themes/epik/assets/functions/theme-options-page.php
+++ wp-content/themes/epik/assets/functions/theme-options-page.php
@@ -13,6 +13,22 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
+add_action( 'genesis_before', 'spa_google_tag_manager' );
+/**
+ * Add required Google Tag Manager script
+ * after the opening <body> tag. Needed for
+ * Google Tag Manager for WordPress plugin.
+ *
+ * @return        void
+ * @author        Joe Dooley
+ *
+ */
+function spa_google_tag_manager() {
+	if ( function_exists( 'gtm4wp_the_gtm_tag' ) ) {
+		gtm4wp_the_gtm_tag();
+	}
+}
+
 
 // ACF Theme Options Page
 if ( function_exists( 'acf_add_options_page' ) || is_admin() ) {
@@ -41,45 +57,27 @@ function spa_free_shipping_before_header() {
 	echo '<section class="before-header"><div class="wrap">';
 
 	genesis_widget_area( 'before-header-left', array(
-		'before' => '<div class="before-header-left-container">',
-		'after'  => '</div>',
+		'before' => '<div class="before-header-left-container"><div class="one-third first before-header-widget before-header-left">',
+		'after'  => '</div></div>',
 	) );
 
 	if ( get_field( 'free_shipping_notification', 'option' ) ) {
 
 		echo '<div class="before-header-middle-container"><div class="before-header-widget one-third before-header-middle">';
 
-		the_field( 'free_shipping_notification', 'option' );
+		echo '<h5 class="free-shipping">' . the_field( 'free_shipping_notification', 'option' ) . '</h5>';
 
 		echo '</div></div>';
 
 	}
 
 	genesis_widget_area( 'before-header-right', array(
-		'before' => '<div class="before-header-right-container">',
-		'after'  => '</div>',
+		'before' => '<div class="before-header-right-container"><div class="one-third before-header-widget before-header-right">',
+		'after'  => '</div></div>',
 	) );
 
 	echo '</div></section>';
-}
-
-
-
-
-add_action( 'genesis_before', 'spa_google_tag_manager' );
-/**
- * Add required Google Tag Manager script
- * after the opening <body> tag. Needed for
- * Google Tag Manager for WordPress plugin.
- *
- * @return        void
- * @author        Joe Dooley
- *
- */
-function spa_google_tag_manager() {
-	if ( function_exists( 'gtm4wp_the_gtm_tag' ) ) {
-		gtm4wp_the_gtm_tag();
-	}
+	
 }
 
 
